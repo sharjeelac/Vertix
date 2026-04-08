5fa81c94cf285b432cec67f1a4b8c34402ef3014
Sharjeel Khalid <sharjeelac@gmail.com>
2026-04-08 08:58:32 +0500

    setup register page

---
+++ app/register/page.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+const RegisterPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      if (password !== confirmPassword) {
+        alert("Passwords do not match");
+        return;
+      }
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+
+      if (!res.ok) {
+        alert("An error occurred during registration");
+      }
+
+      const data = await res.json();
+      if (data.success) {
+        alert("Registration successful! Please log in.");
+        router.push("/login");
+      }
+    } catch (error) {
+      console.log(error, "Error during registration");
+      alert("An error occurred during registration");
+    }
+  };
+  return (
+    <div className="">
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Register</button>
+      </form>
+      <p>Already have an account? <a href="/login">Login</a></p>
+    </div>
+  );
+};
+
+export default RegisterPage;
